c79293bfd9b54fd54eb0eacced2b0286f6ebba8d
shalini <shalini.v@tvsnext.io>
2021-06-30 13:43:36 +0530

    [shalini]UC-6NoOfTimes

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -40,7 +40,7 @@ namespace SnakeAndLadder
                         playerPos -= rollDice;
                     }
              }
-             else if(playerPos +rollDice == 100 && list[pos]=="Ladder")
+             else if(playerPos +rollDice == 100 )
              {
                 playerPos += rollDice;
              
@@ -55,7 +55,7 @@ namespace SnakeAndLadder
              Console.WriteLine("Player current position : " + playerPos);
              count++;
              Console.WriteLine("Final position is :" + playerPos);
-             Console.WriteLine("Total MOves:" + count);
+             Console.WriteLine("number of times:" + count);
         }
         
     }
